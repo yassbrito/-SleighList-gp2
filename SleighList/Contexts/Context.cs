e196816521f156abfdd3ab0f5b5272312adbc0ae
Anna Araujo <annaclaraaraujo229@gmail.com>
2024-12-12 13:39:05 -0300

    projeto final

---
+++ SleighList/Contexts/Context.cs
@@ -0,0 +1,11 @@
+using SleighList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SleighList.Contexts 
+{
+    public class Context{
+        
+
+
+    }
+}
---
+++ SleighList/Controllers/EditarController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SleighList.Controllers
+{
+    [Route("[controller]")]
+    public class EditarController : Controller
+    {
+        private readonly ILogger<EditarController> _logger;
+
+        public EditarController(ILogger<EditarController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
