63235c2449ac6a1decd81ae4893e3aa006de67e4
Anna Araujo <annaclaraaraujo229@gmail.com>
2024-12-12 14:29:43 -0300

    atualizando

--- SleighList/Controllers/LoginController.cs
+++ SleighList/Controllers/LoginController.cs
@@ -1,10 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SleighList.Contexts;
+using SleighList.Models;
 
 namespace SleighList.Controllers
 {
@@ -12,21 +12,53 @@ namespace SleighList.Controllers
     public class LoginController : Controller
     {
         private readonly ILogger<LoginController> _logger;
+        private readonly Context _context;
 
-        public LoginController(ILogger<LoginController> logger)
+        public LoginController(ILogger<LoginController> logger, Context context)
         {
             _logger = logger;
+            _context = context;
         }
 
+        // Action para exibir a página de login
         public IActionResult Index()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // Action para fazer login
+        [Route("Login")]
+        public IActionResult Logar(IFormCollection form)
         {
-            return View("Error!");
+            // Recebe os dados do formulário
+            string emailInformado = form["Email"].ToString();
+            string senhaInformada = form["Senha"].ToString();
+
+            // Busca o usuário no banco de dados
+            Usuario usuarioBuscado = _context.Usuario
+                .FirstOrDefault(u => u.Email == emailInformado && u.Senha == senhaInformada);
+
+            if (usuarioBuscado == null)
+            {
+                // Caso o usuário não seja encontrado, exibe mensagem de erro
+                ViewBag.Message = "Dados inválidos!";
+                return View("Index");
+            }
+            else
+            {
+                // Caso o usuário seja encontrado, armazena os dados na sessão
+                HttpContext.Session.SetString("UsuarioID", usuarioBuscado.UsuarioID.ToString());
+                // Aqui você pode colocar lógica para verificar se o usuário é admin
+                HttpContext.Session.SetString("UsuarioNome", usuarioBuscado.Nome);
+                return LocalRedirect("~/Home/Index"); // Redireciona para a página inicial ou outra página
+            }
         }
+
+        // Caso precise de uma página de erro
+        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // public IActionResult Error()
+        // {
+        //     return View("Error");
+        // }
     }
 }
